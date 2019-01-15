529a8531a97695ce17e06be6131a1bd2b5f17b4d
Administrator <admin@example.com>
2019-01-14 20:01:10 -0600

    Init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		System.out.println("MESSAGE CALLED");
+		return this.message;
+	}
+}
